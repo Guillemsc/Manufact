62254659fccc0be51736fc57263afe382caabb6e
Guillem Sunyer Caldú <gsunyercaldu@gmail.com>
2018-05-26 13:34:19 +0200

    Revert "Working on grid"
    This reverts commit 7278449848156a64c050fbdb3ebe398a76800f51.

--- Manufact/Assets/Scripts/Grid/GridManager.cs
+++ Manufact/Assets/Scripts/Grid/GridManager.cs
@@ -2,60 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class GridManager : Singleton<GridManager>
 {
-    [SerializeField] [HideInInspector] private Grid grid = null;
+    public class Grid
+    {
+        public Grid()
+        {
+
+        }
+
+        private List<GridSlot> grid_slots = new List<GridSlot>();
+    }
+
+    public class GridSlot
+    {
+        public GridSlot()
+        {
+
+        }
+
+        private GameObject game_object = null;
+        private SpriteRenderer sprite_renderer = null;
+        private Collider2D collider = null;
+        private float grid_size = 0.0f;
+    }
 
     private void Awake()
     {
         InitInstance(this, gameObject);
-
-        CheckGridInstance();
     }
 
-    private void Update()
+    private void Start ()
     {
-        CheckGridInstance();
-    }
+		
+	}
 
-    public Grid GetGrid()
-    {
-        return grid;
-    }
-
-    private void CheckGridInstance()
-    {
-        Grid[] grids = FindObjectsOfType<Grid>();
-
-        List<Grid> grids_list = new List<Grid>();
-        grids_list.AddRange(grids);
-
-        if(grids_list.Count == 0)
-        {
-            GameObject go = new GameObject();
-            go.name = "GRID";
-            grid = go.AddComponent<Grid>();
-            go.transform.parent = transform;
-        }
-        else if(grids_list.Count == 1)
-        {
-            if (grid != null)
-            {
-                grid.transform.parent = null;
-                grid.gameObject.name = "GRID";
-            }
-        }
-        else if(grids_list.Count > 1)
-        {
-            while (grids_list.Count > 1)
-            {
-                DestroyImmediate(grids_list[grids_list.Count - 1].gameObject);
-                grids_list.Remove(grids_list[grids_list.Count - 1]);
-            }
-
-            Debug.LogWarning("[Grid] Grid creation is restricted, if you want acces to the grid use the GridManager.Instance");
-        }
-    }
-
+    private void Update () {
+		
+	}
 }
